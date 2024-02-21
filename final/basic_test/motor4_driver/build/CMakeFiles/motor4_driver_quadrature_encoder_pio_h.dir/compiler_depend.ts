cbef0b494ef73d5cc576f85d4deca8a8f6b44024
Juan Ramirez Jardua <lch_jramirez@hotmail.com>
2024-02-21 00:07:59 -0300

    .gitignore included

--- final/basic_test/motor4_driver/build/CMakeFiles/motor4_driver_quadrature_encoder_pio_h.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for custom commands dependencies management for motor4_driver_quadrature_encoder_pio_h.
--- prototype/motor6_ROSPicoBridge/build/CMakeFiles/mootor6_ROSPicoBridge.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for mootor6_ROSPicoBridge.
-# This may be replaced when dependencies are built.
